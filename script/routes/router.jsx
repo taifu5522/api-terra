085f1756b85b3c71db320b73e63725d4e838f550
xu.wang <xu.wang@ishansong.com>
2017-04-27 20:23:13 +0800

    particles commit

--- script/routes/router.jsx
+++ script/routes/router.jsx
@@ -1,9 +1,22 @@
+/**
+ * @Author: wangxu <ceekey>
+ * @Date:   2017-04-26 11:15:45
+ * @Email:  xu.wang@ishansong.com
+ * @Project: terra
+ * @Filename: router.jsx
+ * @Last modified by:   ceekey
+ * @Last modified time: 2017-04-27 17:16:21
+ */
+
+
+
 import React from 'react';
 
 //router
 import { Router, Route, Link, hashHistory } from 'react-router';
 
 import Demo from '../pages/demo.jsx';
+import Login from '../pages/user/login.jsx';
 
 export default class Routers extends React.Component{
     constructor(props){
@@ -13,8 +26,8 @@ export default class Routers extends React.Component{
         return (
             <Router history={hashHistory}>
                 <Route path="/" component={Demo}/>
+                <Route path="/login" component={Login}/>
             </Router>
         )
     }
 }
-
